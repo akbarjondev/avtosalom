5bc24cf5a8adca32f738169f7ea92ba3a387dd14
akbarjondev <tojiyev.akbarjon@gmail.com>
2024-02-03 08:57:35 +0500

    Add prettier

--- src/components/TopLinks/TopLinks.tsx
+++ src/components/TopLinks/TopLinks.tsx
@@ -1,21 +1,24 @@
-import { ReactElement } from "react";
-import { HEADER_TOP_LINKS } from "@/config/constants";
-import { useLocale } from "next-intl";
-import Link from "next/link";
-import { Icon } from "../Icon/Icon";
+import { ReactElement } from 'react'
+import { HEADER_TOP_LINKS } from '@/config/constants'
+import { useLocale } from 'next-intl'
+import Link from 'next/link'
+import { Icon } from '../Icon/Icon'
 
 export const TopLinks = () => {
-  const locale = useLocale();
+  const locale = useLocale()
 
   return (
-    <nav className="text-gray-900">
-      <ul className="container flex gap-6 items-center font-normal">
-        {HEADER_TOP_LINKS.map(link => <li key={link.path}>
-          <Link className="flex flex-col items-center" href={link.path}>{link.label[locale]}
+    <nav className='text-gray-900'>
+      <ul className='container flex items-center gap-6 font-normal'>
+        {HEADER_TOP_LINKS.map((link) => (
+          <li key={link.path}>
+            <Link className='flex flex-col items-center' href={link.path}>
+              {link.label[locale]}
               <Icon icon={link.icon} />
-          </Link>
-          </li>)}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
-  );
+  )
 }
